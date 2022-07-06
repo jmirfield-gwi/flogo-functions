5abbc41396ce62df37f189f80f2e7c24eecb89dd
jtmirfield <jmirfield@staff.gwi.net>
2022-07-06 12:16:06 -0400

    Added more test coverage

--- datetime/formatUnixTime_test.go
+++ datetime/formatUnixTime_test.go
@@ -10,9 +10,23 @@ func init() {
 	function.ResolveAliases()
 }
 
-func TestFnFormatUnixTime_Eval(t *testing.T) {
+func TestSuccessfulFormatUnixTime_Eval(t *testing.T) {
 	var in = &FormatUnixTime{}
 	final, err:= in.Eval("2022-06-19T19:48:37.073976111Z")
 	assert.Nil(t, err)
 	assert.Equal(t, "1655668117", final)
+}
+
+func TestFailingFormatTooShort_Eval(t *testing.T) {
+	var in = &FormatUnixTime{}
+	final, err:= in.Eval("123")
+	assert.Nil(t, final)
+	assert.Equal(t, err.Error(), "parse [123] to time error: unrecognized format, too short 123")
+}
+
+func TestFailingInvalidCharacters_Eval(t *testing.T) {
+	var in = &FormatUnixTime{}
+	final, err:= in.Eval("abc")
+	assert.Nil(t, final)
+	assert.Equal(t, err.Error(), "parse [abc] to time error: Could not find format for \"abc\"")
 }
